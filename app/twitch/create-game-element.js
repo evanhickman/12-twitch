a94e41bedfa7b8b4e35eb5f4c33d1e057288cd5e
Evan Hickman <travisevanhickman@gmail.com>
2017-02-17 15:59:48 -0600

    pass all number 4 tests

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -3,10 +3,12 @@ export default function createGameItem(game) {
   el.classList.add('game-item');
   el.innerHTML = `
   <h3 class="game-item__name"></h3>
-  <h4 class="game-item__popularity"></h4>`;
+  <h4 class="game-item__popularity"></h4>
+  <img class="game-item__pic">`;
   el.querySelector('.game-item__name').innerText = `${game.name}`;
   el.querySelector('.game-item__popularity').innerText = `${game.popularity} Viewers`;
-
+  el.querySelector('.game-item__pic').src = game.box.large;
+  el.querySelector('.game-item__pic').alt = game.name;
 
   return el;
 }
