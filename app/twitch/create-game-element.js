a2b47eb81e7cbed4bfba7fc32528fed81953d394
Evan Hickman <travisevanhickman@gmail.com>
2017-02-21 15:37:20 -0600

    fix app main grid

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -2,9 +2,9 @@ export default function createGameElement(game) {
   const el = document.createElement('div');
   el.classList.add('game-item');
   el.innerHTML = `
+  <img class="game-item__pic">
   <h3 class="game-item__name"></h3>
-  <h4 class="game-item__popularity"></h4>
-  <img class="game-item__pic">`;
+  <h4 class="game-item__popularity"></h4>`;
   el.querySelector('.game-item__name').innerText = `${game.name}`;
   el.querySelector('.game-item__popularity').innerText = `${game.popularity} Viewers`;
   el.querySelector('.game-item__pic').src = game.box.large;
